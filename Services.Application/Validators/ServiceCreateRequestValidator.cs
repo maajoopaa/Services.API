16c3ded61591b73f549989cea20bb8904ddc1093
Opa Maj <saintkilla79@gmail.com>
2025-05-27 16:12:19 +0300

    add models

---
+++ Services.Application/Validators/ServiceCreateRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Services.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Application.Validators
+{
+    public class ServiceCreateRequestValidator : AbstractValidator<ServiceCreateRequest>
+    {
+        public ServiceCreateRequestValidator()
+        {
+            
+        }
+    }
+}
