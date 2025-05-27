da0b906b4944bb1f8d34b49e2db3cd9161dfc05b
Opa Maj <saintkilla79@gmail.com>
2025-05-27 16:13:36 +0300

    add models

---
+++ Services.Application/Models/ServiceCreateRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Application.Models
+{
+    public class ServiceCreateRequest
+    {
+        public string Name { get; set; } = null!;
+
+        public double Price { get; set; } = 0;
+
+        public Guid CategoryId { get; set; }
+
+        public bool IsActive { get; set; } = false;
+    }
+}
---
+++ Services.Application/Models/SpecializationUpdateRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Application.Models
+{
+    public class SpecializationUpdateRequest
+    {
+        public string Name { get; set; } = null!;
+
+        public bool IsActive { get; set; } = false;
+
+        public List<Guid> ServiceIds { get; set; } = [];
+    }
+}
