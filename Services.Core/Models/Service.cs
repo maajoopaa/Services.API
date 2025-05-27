60bb6d277827a6acd1b628906f9f489f999d9fda
Opa Maj <saintkilla79@gmail.com>
2025-05-27 15:14:03 +0300

    create models in core

---
+++ Services.Core/Models/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Core.Models
+{
+    public class Service
+    {
+        public Guid Id { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public double Price { get; set; } = 0;
+
+        public Guid SpecializationId { get; set; }
+
+        public bool IsActive { get; set; } = false;
+    }
+}
---
+++ Services.Core/Models/Specialization.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Core.Models
+{
+    public class Specialization
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public bool IsActive { get; set; } = false; 
+    }
+}
