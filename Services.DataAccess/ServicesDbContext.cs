8768d8b936cce07e5381c7aec336159cbc505a6d
Opa Maj <saintkilla79@gmail.com>
2025-05-27 15:14:52 +0300

    add db context

---
+++ Services.DataAccess/ServicesDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DataAccess
+{
+    public class ServicesDbContext : DbContext
+    {
+        public ServicesDbContext(DbContextOptions<ServicesDbContext> options)
+            : base(options) { }
+
+        public DbSet<Specialization> Specializations { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Service> Services { get; set; }
+    }
+}
