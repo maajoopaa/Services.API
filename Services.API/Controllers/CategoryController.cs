5e84b083bbfd5ce2a47b305579f079b6bc134bb8
Opa Maj <saintkilla79@gmail.com>
2025-05-28 13:45:30 +0300

    add controllers

---
+++ Services.API/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Application.Interfaces;
+using Services.Core.Models;
+
+namespace Services.API.Controllers
+{
+    [ApiController]
+    [Route("catrgory")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var response = await _categoryService.GetAsync(id, cancellationToken);
+
+            return Ok(response);
+        }
+
+        [HttpPost("list")]
+        public async Task<IActionResult> GetAllAsync([FromBody] PaginationModel model, CancellationToken cancellationToken)
+        {
+            var response = await _categoryService.GetAllAsync(model, cancellationToken);
+
+            return Ok(response);
+        }
+    }
+}
--- Services.API/Controllers/ServiceController.cs
+++ Services.API/Controllers/ServiceController.cs
@@ -3,33 +3,34 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Services.Application.Interfaces;
 using Services.Application.Models;
+using Services.Application.Services;
 using Services.Core.Models;
 
 namespace Services.API.Controllers
 {
     [ApiController]
-    [Route("specialization")]
-    public class SpecializationController : ControllerBase
+    [Route("service")]
+    public class ServiceController : ControllerBase
     {
-        private readonly ISpecializationService _specializationService;
+        private readonly IServiceService _serviceService;
 
-        public SpecializationController(ISpecializationService specializationService)
+        public ServiceController(IServiceService serviceService)
         {
-            _specializationService = specializationService;
+            _serviceService = serviceService;
         }
 
-        [HttpPost(""),Authorize(Roles ="Admin")]
-        public async Task<IActionResult> CreateAsync([FromBody] SpecializationCreateRequest model, CancellationToken cancellationToken)
+        [HttpPost(""), Authorize(Roles = "Admin")]
+        public async Task<IActionResult> CreateAsync([FromBody] ServiceCreateRequest model, CancellationToken cancellationToken)
         {
-            await _specializationService.CreateAsync(model, cancellationToken);
+            await _serviceService.CreateAsync(model, cancellationToken);
 
             return Ok();
         }
 
         [HttpPut("{id:guid}"), Authorize(Roles = "Admin")]
-        public async Task<IActionResult> UpdateAsync(Guid id, [FromBody] SpecializationUpdateRequest model, CancellationToken cancellationToken)
+        public async Task<IActionResult> UpdateAsync(Guid id, [FromBody] ServiceUpdateRequest model, CancellationToken cancellationToken)
         {
-            await _specializationService.UpdateAsync(id, model, cancellationToken);
+            await _serviceService.UpdateAsync(id, model, cancellationToken);
 
             return Ok();
         }
@@ -37,7 +38,7 @@ namespace Services.API.Controllers
         [HttpPatch("{id:guid}/{isActive:bool}"), Authorize(Roles = "Admin")]
         public async Task<IActionResult> UpdateStatusAsync(Guid id, bool isActive, CancellationToken cancellationToken)
         {
-            await _specializationService.UpdateStatusAsync(id, isActive, cancellationToken);
+            await _serviceService.UpdateStatusAsync(id, isActive, cancellationToken);
 
             return Ok();
         }
@@ -45,7 +46,7 @@ namespace Services.API.Controllers
         [HttpPost("list"), Authorize]
         public async Task<IActionResult> GetAllAsync([FromBody] PaginationModel model, CancellationToken cancellationToken)
         {
-            var response = await _specializationService.GetAllAsync(model, cancellationToken);
+            var response = await _serviceService.GetAllAsync(model, cancellationToken);
 
             return Ok(response);
         }
@@ -53,7 +54,7 @@ namespace Services.API.Controllers
         [HttpGet("{id:guid}")]
         public async Task<IActionResult> GetAsync(Guid id, CancellationToken cancellationToken)
         {
-            var response = await _specializationService.GetAsync(id, cancellationToken);
+            var response = await _serviceService.GetAsync(id, cancellationToken);
 
             return Ok(response);
         }
